8b8c75514dddfb16d8780cc52f246e214877b040
AlessandroMilani <alessandromilani2000@gmail.com>
2024-05-03 19:10:05 +0200

    chore: Update start.bat to fix PYTHON path issue

--- start.bat
+++ start.bat
@@ -1,9 +1,8 @@
 @echo off
 
-set PYTHON=./venv/Scripts/python.exe
+set PYTHON=%~dp0/venv/Scripts/python.exe
 set GIT=
 set VENV_DIR=./venv
 set COMMANDLINE_ARGS=
 
-# execute the python script
 %PYTHON% main.py %COMMANDLINE_ARGS%
