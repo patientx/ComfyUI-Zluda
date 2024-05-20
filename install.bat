c716041297f46dd84d54ac5748b5cd3ad954bf6a
patientx <patient.x@gmail.com>
2024-05-20 19:48:04 +0300

    Update install.bat
    zluda patch

--- install.bat
+++ install.bat
@@ -36,6 +36,16 @@ cd ..
 echo ..................................................... 
 echo *** "Patching ZLUDA"
 echo.
+rmdir /S /q zluda
+curl -s -L https://github.com/lshqqytiger/ZLUDA/releases/download/rel.2804604c29b5fa36deca9ece219d3970b61d4c27/ZLUDA-windows-amd64.zip > zluda.zip
+tar -xf zluda.zip
+del zluda.zip
+copy zluda\cublas.dll venv\Lib\site-packages\torch\lib\cublas64_11.dll /y
+copy zluda\cusparse.dll venv\Lib\site-packages\torch\lib\cusparse64_11.dll /y
+copy zluda\nvrtc.dll venv\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y
+echo.
+@echo zluda patched.
+echo.
 echo ..................................................... 
 echo *** Installation is done. You can start.bat to start the app later. 
 echo *** For now app is going to start for the first time.
