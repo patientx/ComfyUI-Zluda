5e908e08e9fc0750826ccac109577a953d7bf4dd
patientx <patient.x@gmail.com>
2024-06-22 14:45:56 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,6 +3,6 @@
 set PYTHON=%~dp0/venv/Scripts/python.exe
 set GIT=
 set VENV_DIR=./venv
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--auto-launch
 
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
