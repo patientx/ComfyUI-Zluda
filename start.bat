cc93057c6caaabce1c09d098a078b29ae6753197
patientx <patient.x@gmail.com>
2024-06-25 16:33:22 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -5,5 +5,7 @@ set GIT=
 set VENV_DIR=./venv
 set COMMANDLINE_ARGS=--auto-launch
 
+echo *** Checking and updating to new version if possible 
 git pull
+echo.
 .\zluda\zluda.exe -- %PYTHON% main.py %COMMANDLINE_ARGS%
