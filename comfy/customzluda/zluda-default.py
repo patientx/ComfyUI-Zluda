c1d7452bf86b96bcc4711b018e707b8a9cfc22eb
patientx <patient.x@gmail.com>
2026-01-08 13:17:59 +0300

    Refactor ZLUDA detection and package management
    Refactor ZLUDA detection and package management logic, including compatibility checks and environment variable handling.

--- comfy/customzluda/zluda-default.py
+++ comfy/customzluda/zluda-default.py
@@ -251,7 +251,7 @@ packages_to_monitor = [
     "comfyui-embedded-docs",
     "pydantic",
     "pydantic-settings",
-    "comfy-kitchen",
+    # "comfy-kitchen", (it seems it is not compatible with the old torch 2.3 we have been using)
 ]
 
 print("\n  ::  Checking package versions...")
@@ -447,6 +447,7 @@ else:
     print(f"  ::  CUDA device detected: {zluda_device_name or 'None'}")
     print("***--------------------------------------------------------***\n")
 # ------------------- End Zluda detection -------------------
+
 
 
 
