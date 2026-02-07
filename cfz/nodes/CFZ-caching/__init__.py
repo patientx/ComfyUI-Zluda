39a077d67cfd87509c5c5004198f93e94a9bcfb8
patientx <patient.x@gmail.com>
2026-02-07 20:01:11 +0300

    Add CFZ_CUDNN to node class and display mappings

--- cfz/nodes/CFZ-caching/__init__.py
+++ cfz/nodes/CFZ-caching/__init__.py
@@ -1,13 +1,15 @@
-from .cfz_caching_condition import save_conditioning, load_conditioning, CFZ_PrintMarker
+from .cfz_caching_condition import save_conditioning, load_conditioning, CFZ_PrintMarker, CFZ_CUDNN
 
 NODE_CLASS_MAPPINGS = {
     "CFZ_save_conditioning": save_conditioning,
     "CFZ_load_conditioning": load_conditioning,
     "CFZ_PrintMarker": CFZ_PrintMarker,   # NEW
+    "CFZ_CUDNN": CFZ_CUDNN,   # NEW
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CFZ_save_conditioning": "CFZ Save Conditioning",
     "CFZ_load_conditioning": "CFZ Load Conditioning",
     "CFZ_PrintMarker": "CFZ Print Marker",
+    "CFZ_CUDNN": "CFZ CUDNN",   # NEW
 }
