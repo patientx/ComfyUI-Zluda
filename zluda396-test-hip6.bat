0c6ca3ba060f852db8be25892527632df8c7bb88
patientx <patient.x@gmail.com>
2026-02-11 18:42:25 +0300

    Add files via upload

---
+++ zluda396-test-hip6.bat
@@ -0,0 +1,14 @@
+echo :: ZLUDA version 3.9.6 patcher for hip 6.x ::
+rmdir /S /Q zluda 2>nul
+%SystemRoot%\system32\curl.exe -sL --ssl-no-revoke https://github.com/lshqqytiger/ZLUDA/releases/download/rel.854c58e1565c3597e17046d7cc2b1eab96fcdf55/ZLUDA-windows-amd64.zip > zluda.zip
+%SystemRoot%\system32\tar.exe -xf zluda.zip
+del zluda.zip
+echo :: Patch DLLs ::
+copy zluda\cublas.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cublas64_11.dll /y
+copy zluda\cusparse.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cusparse64_11.dll /y
+copy %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc_cuda.dll /y
+copy zluda\nvrtc.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\nvrtc64_112_0.dll /y
+copy zluda\cufft.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cufft64_10.dll /y
+copy zluda\cufftw.dll %VIRTUAL_ENV%\Lib\site-packages\torch\lib\cufftw64_10.dll /y
+echo :: Zluda 3.9.6 installed. ::
+pause
